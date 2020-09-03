e857b455a14fd966849622d4e66a0afc26f63d46
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-04 01:04:21 +0530

    feat: Include sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import {
     Switch,
     Route,
 } from 'react-router-dom';
+import {
+    Row,
+    Col,
+} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 
@@ -11,21 +16,30 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
 
+import Sidebar from './components/Sidebar';
+
 function App() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/about">
-                    <About />
-                </Route>
-                <Route path="/login">
-                    <Login />
-                </Route>
-                <Route path="/">
-                    <Home />
-                </Route>
-            </Switch>
-        </Router>
+        <Row>
+            <Col md="2">
+                <Sidebar />
+            </Col>
+            <Col>
+                <Router>
+                    <Switch>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/login">
+                            <Login />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </Router>
+            </Col>
+        </Row>
     );
 }
 
