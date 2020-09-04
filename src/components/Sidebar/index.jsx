cd5dfb87c546c97ce4e861330d684a06980a115d
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-04 15:24:58 +0530

    feat: Color changed for sidebar items

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -9,19 +9,19 @@ export default function Sidebar() {
             className="d-md-block bg-dark sidebar"
         >
             <Nav.Link href="/">
-                <Navbar.Brand>C7F</Navbar.Brand>
+                <Navbar.Brand className="text-light">C7F</Navbar.Brand>
             </Nav.Link>
             <Nav.Item>
-                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/" className="text-light">Home</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="/login">Login</Nav.Link>
+                <Nav.Link href="/login" className="text-light">Login</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="/register">Register</Nav.Link>
+                <Nav.Link href="/register" className="text-light">Register</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="/challenges">Challenges</Nav.Link>
+                <Nav.Link href="/challenges" className="text-light">Challenges</Nav.Link>
             </Nav.Item>
         </Nav>
     );
