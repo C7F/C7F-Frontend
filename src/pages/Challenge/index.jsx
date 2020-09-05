0106d5b5a65a7658937d52b2182234e8e8b19cd6
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-06 05:13:29 +0530

    feat: Added basic challenge page

--- src/pages/Challenge/index.jsx
+++ src/pages/Challenge/index.jsx
@@ -3,6 +3,8 @@ import { useParams, useHistory } from 'react-router-dom';
 
 import Card from '../../components/Card';
 
+import './style.scss';
+
 export default function Challenge() {
     const { id } = useParams();
     const history = useHistory();
@@ -12,13 +14,15 @@ export default function Challenge() {
     }
 
     return (
-        <Card>
-            <Card.Header>
-                Challenge Name
-            </Card.Header>
-            <Card.Body>
-                Challenge Description
-            </Card.Body>
-        </Card>
+        <div className="mt-5 challenge-container">
+            <Card className="challenge-card m-auto">
+                <Card.Header>
+                    Challenge Name
+                </Card.Header>
+                <Card.Body>
+                    Challenge Description
+                </Card.Body>
+            </Card>
+        </div>
     );
 }
