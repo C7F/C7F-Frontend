fd6a6a91bfb802214dc8df3379fce673136015e2
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-06 05:56:13 +0530

    feat: Added countdown to sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
+import Countdown from '../Countdown';
+
 import './style.scss';
 
 export default function Sidebar() {
     return (
-        <Nav
-            className="d-md-block bg-dark sidebar"
-        >
-            <Nav.Link href="/">
-                <Navbar.Brand className="text-light">C7F</Navbar.Brand>
-            </Nav.Link>
-            <Nav.Item>
-                <Nav.Link href="/" className="text-light">Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="/login" className="text-light">Login</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="/register" className="text-light">Register</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="/challenge" className="text-light">Challenges</Nav.Link>
-            </Nav.Item>
-        </Nav>
+        <div>
+            <Nav
+                className="d-md-block bg-dark sidebar"
+            >
+                <Nav.Link href="/">
+                    <Navbar.Brand className="text-light">C7F</Navbar.Brand>
+                </Nav.Link>
+                <Nav.Item>
+                    <Nav.Link href="/" className="text-light">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="/login" className="text-light">Login</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="/register" className="text-light">Register</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="/challenge" className="text-light">Challenges</Nav.Link>
+                </Nav.Item>
+                <Countdown date="10/1/2020" />
+            </Nav>
+        </div>
     );
 }
