d31138b7b2cc3a3bb1129c74fea51ca4d9cfbc00
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-06 05:51:25 +0530

    feat: Remove countdown from homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,66 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './style.scss';
-import { nanoid } from '@reduxjs/toolkit';
 import Typist from 'react-typist';
 
 export default function Home() {
-    const calculateTimeLeft = () => {
-        const year = new Date().getFullYear();
-        const difference = +new Date(`10/01/${year}`) - +new Date();
-        let timeLeft = {};
-
-        if (difference > 0) {
-            timeLeft = {
-                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-                minutes: Math.floor((difference / 1000 / 60) % 60),
-                seconds: Math.floor((difference / 1000) % 60),
-            };
-        }
-
-        return timeLeft;
-    };
-
-    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setTimeLeft(calculateTimeLeft());
-        }, 1000);
-        return () => clearTimeout(timer);
-    });
-
-    const timerComponents = [];
-
-    Object.keys(timeLeft).forEach((interval) => {
-        if (!timeLeft[interval]) {
-            return;
-        }
-
-        timerComponents.push(
-            <span key={nanoid()}>
-                {timeLeft[interval]}
-                {' '}
-                {interval}
-                {' '}
-            </span>,
-        );
-    });
     return (
         <div>
-            <h1 className="">
+            <h1>
                 <Typist>
                     Hey!
                     <Typist.Backspace count={4} delay={500} />
                     Welcome to C7F2020
                 </Typist>
             </h1>
-            <div className="text-center mt-5">
-                <h3>
-                    Countdown
-                </h3>
-                {timerComponents.length ? timerComponents : <span>It is time!</span>}
-            </div>
         </div>
 
     );
