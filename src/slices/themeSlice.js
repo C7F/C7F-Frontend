1c1204839eb429172ec43f74d99d8228b84ddbac
SamSan123 <sanyamagrawal3@gmail.com>
2020-09-07 03:26:44 +0530

    feat: Added new themes

--- src/slices/themeSlice.js
+++ src/slices/themeSlice.js
@@ -10,13 +10,67 @@ const THEMES = {
         default: '#44475a',
         warning: '',
     },
+    vs2015: {
+        fgColor: '#1e1e1e',
+        bgColor: '#b5aea5',
+        dark: '#353d42',
+        primary: '#7a7676',
+        error: '#660000',
+        default: '#44475a',
+        warning: '',
+    },
+    'atom-one-dark': {
+        fgColor: '#09868b',
+        bgColor: '#c7cc93',
+        dark: '#82716e',
+        primary: '#acb7ae',
+        error: '#ff5555',
+        default: '#44475a',
+        warning: '',
+    },
+    Monokai: {
+        fgColor: '#09868b',
+        bgColor: '#f7f7f7',
+        dark: '#82716e',
+        primary: '#acb7ae',
+        error: '#ff5555',
+        default: '#44475a',
+        warning: '',
+    },
+    'gruvbox-dark': {
+        fgColor: '#09868b',
+        bgColor: '#f7f7f7',
+        dark: '#82716e',
+        primary: '#acb7ae',
+        error: '#ff5555',
+        default: '#44475a',
+        warning: '',
+    },
+    'solarized-light': {
+        fgColor: '#09868b',
+        bgColor: '#f7f7f7',
+        dark: '#82716e',
+        primary: '#acb7ae',
+        error: '#ff5555',
+        default: '#44475a',
+        warning: '',
+    },
+    'tomorrow-night': {
+        fgColor: '#09868b',
+        bgColor: '#f7f7f7',
+        dark: '#82716e',
+        primary: '#5b807a',
+        error: '#ff5555',
+        default: '#44475a',
+        warning: '',
+    },
 };
 
 const themeSlice = createSlice({
     name: 'theme',
     initialState: {
         name: 'dracula',
-        ...THEMES.dracula,
+        ...THEMES['atom-one-dark'],
     },
     reducers: {
         themeUpdate(state, action) {
