8d7c10bfbe707a19e86ae799841b6833bf8def94
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-07 21:35:10 +0530

    feat: Flag input added

--- src/pages/Challenge/index.jsx
+++ src/pages/Challenge/index.jsx
@@ -1,13 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 
 import Highlight from '../../components/Highlight';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import './style.scss';
 
 export default function Challenge() {
     const { id } = useParams();
     const history = useHistory();
+
+    const [flag, setFlag] = useState('');
+
+    const changeFlag = ({ target }) => {
+        setFlag(target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
 
     useEffect(() => {
         if (!id) {
@@ -27,6 +39,15 @@ Challenge: [Oreo](http://chall.csivit.com:30243)
             <Highlight>
                 {sampleChallenge}
             </Highlight>
+            <form onSubmit={handleSubmit} className="flag-form">
+                <div className="form-group">
+                    <label htmlFor="teamName">
+                        Flag
+                    </label>
+                    <Input id="flag" type="text" value={flag} onChange={changeFlag} placeholder="Enter the flag" required />
+                </div>
+                <Button type="submit" btnType="primary">Submit</Button>
+            </form>
         </div>
     );
 }
