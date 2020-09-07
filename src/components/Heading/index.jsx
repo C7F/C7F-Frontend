4ccc00aa8c267b715dda8e909cbd0255ac6966d7
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-07 13:15:13 +0530

    feat: Added heading component

---
+++ src/components/Heading/index.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Heading1 = styled.h1`
+    color: ${(props) => props.theme.primary} !important;
+`;
+
+export const Heading2 = styled.h2`
+    color: ${(props) => props.theme.primary} !important;
+`;
