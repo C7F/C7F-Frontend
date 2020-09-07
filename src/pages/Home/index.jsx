eba76d6d8d2c605045d2ca286637953f3060abc1
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-07 13:16:44 +0530

    feat: Use heading component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import './style.scss';
 import Typist from 'react-typist';
+
 import Highlight from '../../components/Highlight';
+import { Heading1, Heading2 } from '../../components/Heading';
 
 export default function Home() {
     return (
         <div>
-            <h1>
+            <Heading1>
                 <Typist cursor={{ hideWhenDone: true, hideWhenDoneDelay: 300 }}>
                     Hey!
                     <Typist.Backspace count={4} delay={500} />
                     Welcome to C7F2020
                 </Typist>
-            </h1>
+            </Heading1>
             <article className="home-description">
                 <p>Team C7F is hosting a another CTF!</p>
-                <h2>
+                <Heading2>
                     When?
-                </h2>
+                </Heading2>
                 <p>
                     Start Time:
                     {' '}
@@ -28,11 +30,11 @@ export default function Home() {
                     {' '}
                     {new Date('11/10/2020').toString()}
                 </p>
-                <h2>Where?</h2>
+                <Heading2>Where?</Heading2>
                 <p>
                     The CTF is online and anyone can take part in it!
                 </p>
-                <h2>What?</h2>
+                <Heading2>What?</Heading2>
                 <p>
                     We have challenges of various categories including:
                     <ul>
@@ -45,7 +47,7 @@ export default function Home() {
                         <li>Reversing</li>
                     </ul>
                 </p>
-                <h2>Flag format?</h2>
+                <Heading2>Flag format?</Heading2>
                 <p>
                     The flag is in the following format:
                     <br />
@@ -54,7 +56,7 @@ export default function Home() {
                         {'C7F{some_string_here}'}
                     </Highlight>
                 </p>
-                <h2>Sponsors</h2>
+                <Heading2>Sponsors</Heading2>
                 <p>
                     C7F2020 is sponsored by:
                     <ul>
