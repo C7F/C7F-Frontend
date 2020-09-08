a2a3b5a4b1254f5f08f185c69899cbdc39bc05eb
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-08 18:54:21 +0530

    feat: Save team data in localStorage

--- src/slices/teamSlice.js
+++ src/slices/teamSlice.js
@@ -11,17 +11,44 @@ export const fetchTokenUsingToken = createAsyncThunk(
     async ({ loginToken }) => requestTokenLogin({ loginToken }),
 );
 
+const getInitialTeam = (attribute, type) => {
+    const currentTeam = JSON.parse(localStorage.getItem('team'));
+
+    let fallback = '';
+    if (type === 'boolean' || type === 'bool') {
+        fallback = false;
+    } else if (type === 'number') {
+        fallback = 0;
+    }
+
+    return currentTeam ? currentTeam[attribute] || fallback : fallback;
+};
+
+const saveTeam = (attribute) => {
+    const currentTeam = JSON.parse(localStorage.getItem('team'));
+    localStorage.setItem('team', JSON.stringify({ ...currentTeam, ...attribute }));
+};
+
 const teamSlice = createSlice({
     name: 'team',
     initialState: {
-        name: '',
-        points: 0,
-        loggedIn: false,
-        token: '',
+        name: getInitialTeam('name'),
+        points: getInitialTeam('points', 'number'),
+        loggedIn: getInitialTeam('loggedIn', 'boolean'),
+        token: getInitialTeam('token'),
     },
     reducers: {
         nameUpdate(state, action) {
             state.name = action.payload;
+            saveTeam({ name: action.payload });
+        },
+        tokenUpdate(state, action) {
+            state.token = action.payload;
+            saveTeam({ token: action.payload });
+        },
+        loggedInUpdate(state, action) {
+            state.loggedIn = action.payload;
+            saveTeam({ loggedIn: action.payload });
         },
     },
     extraReducers: {
@@ -32,6 +59,7 @@ const teamSlice = createSlice({
             }
             state.token = action.payload;
             state.loggedIn = true;
+            saveTeam({ token: action.payload, loggedIn: true });
         },
         [fetchTokenUsingToken.fulfilled]: (state, action) => {
             if (action.error) {
@@ -39,12 +67,17 @@ const teamSlice = createSlice({
             }
             state.token = action.payload;
             state.loggedIn = true;
+            saveTeam({ token: action.payload, loggedIn: true });
         },
     },
 });
 
 export const selectTeam = (state) => state.team;
 
-export const { nameUpdate } = teamSlice.actions;
+export const {
+    nameUpdate,
+    tokenUpdate,
+    loggedInUpdate,
+} = teamSlice.actions;
 
 export default teamSlice.reducer;
