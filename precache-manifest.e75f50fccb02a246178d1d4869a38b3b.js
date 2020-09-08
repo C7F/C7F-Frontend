aabdfe8ba3dafdc79bf86cfcfe468637c4a1ccd5
roerohan <roerohan@gmail.com>
2020-09-08 19:38:42 +0000

    Deploying to gh-pages from  @ a94d11c34f28f7cef98cdf30e04a4c95d38a557e 🚀

---
+++ precache-manifest.e75f50fccb02a246178d1d4869a38b3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e799c89edc747a7bb0b789516c7b664",
+    "url": "$(basename $(pwd))/index.html"
+  },
+  {
+    "revision": "56e44d7a91d154158528",
+    "url": "$(basename $(pwd))/static/css/2.56526d64.chunk.css"
+  },
+  {
+    "revision": "2859e461a359c53a1b79",
+    "url": "$(basename $(pwd))/static/css/main.d500fff3.chunk.css"
+  },
+  {
+    "revision": "56e44d7a91d154158528",
+    "url": "$(basename $(pwd))/static/js/2.89e0cae5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "$(basename $(pwd))/static/js/2.89e0cae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2859e461a359c53a1b79",
+    "url": "$(basename $(pwd))/static/js/main.f1131dde.chunk.js"
+  },
+  {
+    "revision": "e36c1ecba06a97da1593",
+    "url": "$(basename $(pwd))/static/js/runtime-main.8557c5d3.js"
+  },
+  {
+    "revision": "82cd9f5d6e9f0e3f3de24eaab6671d88",
+    "url": "$(basename $(pwd))/static/media/brown-papersq.82cd9f5d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/C7F-Frontend/precache-manifest.fef1c1753b5292b308e0cef427a56752.js"
+  "$(basename $(pwd))/precache-manifest.e75f50fccb02a246178d1d4869a38b3b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/C7F-Frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("$(basename $(pwd))/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
