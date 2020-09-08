7f2bb2fe55dbe4e250aa8d52b336eef8daa27058
roerohan <roerohan@gmail.com>
2020-09-08 18:21:04 +0000

    Deploying to gh-pages from  @ 3b69e501c5e07676f793d2390aa406e59ead9d70 🚀

---
+++ precache-manifest.a9a6df9766a6ae405d0300c3b669b337.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317d9ba00ca133fe5fe83c1d4d2fba9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "758416fae3cf8d460b86",
+    "url": "/static/css/2.1f4d0a20.chunk.css"
+  },
+  {
+    "revision": "0fcd2be04da59683fda9",
+    "url": "/static/css/main.5f4aee1e.chunk.css"
+  },
+  {
+    "revision": "758416fae3cf8d460b86",
+    "url": "/static/js/2.bf0b6a4c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bf0b6a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fcd2be04da59683fda9",
+    "url": "/static/js/main.4f416c5a.chunk.js"
+  },
+  {
+    "revision": "06a359e0b91a39448332",
+    "url": "/static/js/runtime-main.eb98b889.js"
+  },
+  {
+    "revision": "82cd9f5d6e9f0e3f3de24eaab6671d88",
+    "url": "/static/media/brown-papersq.82cd9f5d.png"
+  }
+]);
