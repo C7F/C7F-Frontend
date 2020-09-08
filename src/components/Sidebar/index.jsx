336591ea290d1082658a097285c22497b767c063
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-08 18:04:12 +0530

    feat: Challenges show only when logged in

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { nanoid } from '@reduxjs/toolkit';
@@ -7,6 +7,7 @@ import Tree from '../Tree';
 import Countdown from '../Countdown';
 import './style.scss';
 import { getThemeNames, getTheme, themeUpdate } from '../../slices/themeSlice';
+import { selectTeam } from '../../slices/teamSlice';
 
 const ThemeSelector = styled.select`
     color: ${(props) => props.theme.fgColor};
@@ -26,65 +27,80 @@ const SidebarDiv = styled.div`
 export default function Sidebar() {
     const themes = getThemeNames();
     const theme = useSelector(getTheme);
+    const team = useSelector(selectTeam);
+    const [sidebarTree, setSidebarTree] = useState([
+        {
+            text: 'Home',
+            link: '/',
+        }, {
+            text: 'Login',
+            link: '/login',
+        }, {
+            text: 'Register',
+            link: '/register',
+        },
+    ]);
 
     const dispatch = useDispatch();
     const themeSelect = (e) => {
         dispatch(themeUpdate(e.target.value));
     };
+
+    const challengeTree = {
+        text: 'Challenges',
+        children: [
+            {
+                text: 'Pwn',
+                children: [
+                    {
+                        text: 'pwn-intended-0x1',
+                        link: '/challenge/pwn/1',
+                    },
+                    {
+                        text: 'pwn-intended-0x2',
+                        link: '/challenge/pwn/2',
+                    },
+                    {
+                        text: 'pwn-intended-0x3',
+                        link: '/challenge/pwn/3',
+                    },
+                ],
+            },
+            {
+                text: 'Web',
+                children: [
+                    {
+                        text: 'Oreo',
+                        link: '/challenge/web/1',
+                    },
+                    {
+                        text: 'File-Library',
+                        link: '/challenge/web/2',
+                    },
+                    {
+                        text: 'Cascade',
+                        link: '/challenge/web/',
+                    },
+                ],
+            },
+        ],
+    };
+
+    useEffect(() => {
+        if (team.loggedIn && !sidebarTree.filter((item) => item.text === 'Challenges').length) {
+            setSidebarTree(
+                (tree) => tree.filter(
+                    (item) => !['Login', 'Register'].includes(item.text),
+                ).concat(challengeTree),
+            );
+        }
+    }, [team, challengeTree, sidebarTree, setSidebarTree]);
+
     return (
         <SidebarDiv>
             <h2>C7F2020</h2>
             <Countdown date={+new Date('9/10/2020')} />
-            <Tree tree={[
-                {
-                    text: 'Home',
-                    link: '/',
-                }, {
-                    text: 'Login',
-                    link: '/login',
-                }, {
-                    text: 'Register',
-                    link: '/register',
-                }, {
-                    text: 'Challenges',
-                    children: [
-                        {
-                            text: 'Pwn',
-                            children: [
-                                {
-                                    text: 'pwn-intended-0x1',
-                                    link: '/challenge/pwn/1',
-                                },
-                                {
-                                    text: 'pwn-intended-0x2',
-                                    link: '/challenge/pwn/2',
-                                },
-                                {
-                                    text: 'pwn-intended-0x3',
-                                    link: '/challenge/pwn/3',
-                                },
-                            ],
-                        },
-                        {
-                            text: 'Web',
-                            children: [
-                                {
-                                    text: 'Oreo',
-                                    link: '/challenge/web/1',
-                                },
-                                {
-                                    text: 'File-Library',
-                                    link: '/challenge/web/2',
-                                },
-                                {
-                                    text: 'Cascade',
-                                    link: '/challenge/web/',
-                                },
-                            ],
-                        },
-                    ],
-                }]}
-            />
+            <Tree tree={sidebarTree} />
             <div className="themeSelector">
                 <label htmlFor="themeSelect" className="mt-2">Theme</label>
                 <ThemeSelector value={theme.name} id="themeSelect" onChange={themeSelect} className="ml-2">
