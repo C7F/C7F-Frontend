c6a26862eb02b5baf5052f7a2bd9b22fd25d96b5
Rohan Mukherjee <rohanbayern@gmail.com>
2020-10-06 02:37:48 +0530

    feat: Test alert component

--- src/slices/alertsSlice.js
+++ src/slices/alertsSlice.js
@@ -2,11 +2,16 @@ import { createSlice } from '@reduxjs/toolkit';
 import shortid from 'shortid';
 
 const initialState = [
-    {
-        id: shortid(),
-        type: '',
-        message: '',
-    },
+    // {
+    //     id: shortid(),
+    //     type: 'error',
+    //     message: 'Hello world!',
+    // },
+    // {
+    //     id: shortid(),
+    //     type: 'error',
+    //     message: 'Hello world!',
+    // },
 ];
 
 const alertsSlice = createSlice({
@@ -14,7 +19,10 @@ const alertsSlice = createSlice({
     initialState,
     reducers: {
         addAlert(state, action) {
-            state.append(action.payload);
+            state.append({
+                ...action.payload,
+                id: shortid(),
+            });
         },
         removeAlert(state, action) {
             state.filter((item) => item.id !== action.payload);
