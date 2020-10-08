0cecb87b071a8d05d27f9030ca46664d9ec8f2bd
roerohan <roerohan@gmail.com>
2020-10-08 17:53:27 +0000

    Deploying to gh-pages from  @ 5a56728e512c21c623c5176d9e3fd9e34e4f4f88 🚀

---
+++ precache-manifest.ad1b37bbb2913f066e190943f26a154b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "432b0a26ca3bc98f401933a86e630195",
+    "url": "/C7F-Frontend/index.html"
+  },
+  {
+    "revision": "f5e5b581165823de598f",
+    "url": "/C7F-Frontend/static/css/2.d73399ba.chunk.css"
+  },
+  {
+    "revision": "7ec0396b90419fea5f8b",
+    "url": "/C7F-Frontend/static/css/main.d500fff3.chunk.css"
+  },
+  {
+    "revision": "f5e5b581165823de598f",
+    "url": "/C7F-Frontend/static/js/2.3432b5de.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/C7F-Frontend/static/js/2.3432b5de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec0396b90419fea5f8b",
+    "url": "/C7F-Frontend/static/js/main.ea96ae05.chunk.js"
+  },
+  {
+    "revision": "2f8aaf632e85248409e2",
+    "url": "/C7F-Frontend/static/js/runtime-main.72cf65c5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/C7F-Frontend/precache-manifest.08fc0ba76ced08878d5853200049eab9.js"
+  "/C7F-Frontend/precache-manifest.ad1b37bbb2913f066e190943f26a154b.js"
 );
 
 self.addEventListener('message', (event) => {
