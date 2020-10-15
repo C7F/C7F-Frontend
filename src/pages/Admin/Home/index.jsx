d296078e863af0deb903bca9a4a7f0b390f77ca5
Rohan Mukherjee <rohanbayern@gmail.com>
2020-10-16 02:25:28 +0530

    fix: Modified admin home

--- src/pages/Admin/Home/index.jsx
+++ src/pages/Admin/Home/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { Heading1 } from '../../../components/Heading';
 import CategoryPieChart from '../../../components/CategoryPieChart';
+import ChallengeSolvePlot from '../../../components/ChallengeSolvePlot';
 
 export default function Home() {
     return (
         <div>
             <Heading1 type="error">Admin Panel</Heading1>
+            <ChallengeSolvePlot />
             <CategoryPieChart />
         </div>
     );
