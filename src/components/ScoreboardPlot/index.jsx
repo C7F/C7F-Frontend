5a79e71369107e909df425e2d5cc7166d8843128
Rohan Mukherjee <rohanbayern@gmail.com>
2020-10-16 10:53:40 +0530

    feat: Scoreboard graph full width

--- src/components/ScoreboardPlot/index.jsx
+++ src/components/ScoreboardPlot/index.jsx
@@ -59,6 +59,7 @@ export default function ScoreboardPlot(props) {
                     gridcolor: theme.primary,
                 },
             }}
+            useResizeHandler
         />
     );
 }
